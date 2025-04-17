ec2307e68b8ac268612046eb185da14b3db535d2
batman47steam <batman47steam@outlook.com>
2025-04-17 22:15:49 +0800

    add readme

--- step1/train.sh
+++ step1/train.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
-
-CUDA_VISIBLE_DEVICES=0
-python ./train.py
+python train.py
 --datarootTarget
-..\nlos-ot\B
+../Simulate/2D-B1/train/gt
 --datarootData
-..\nlos-ot\B
+../Simulate/2D-B1/train/gt
 --datarootValTarget
-..\nlos-ot\test\gt_val
+../Simulate/2D-B1/test/gt
 --datarootValData
-..\nlos-ot\test\gt_val
+../Simulate/2D-B1/test/gt
 --learn_residual
 --gpu_ids=0
---batchSize=16
---name=local_server # 保存的权重路径是由opt.checkpoints_dir和opt.name决定的，checkpoints_dir一般默认
+--batchSize=8
+--name=local_untrain # 保存权重的路径是有opt.checkpoints_dir和opt.name决定的，checkpoints_dir一般默认
 --niter=100
 --niter_decay=300
 --which_model_netG
@@ -21,9 +19,22 @@ introAE
 --display_port=8097
 --norm
 batch
+--input_height
+48
+--input_width
+48
+--output_height
+48
+--output_width
+48
+--channels
+"64, 128, 256, 512"
 
 # step1最重要的两个问题
 # 保存的结果在哪里 ？
 # 保存的是什么 ？
 # 保存的路径由 opt.checkpoints(默认checkpoints)和opt.name决定
 # base_model的save_network里，epoch_label_net_G_decoder.pth (epoch_label) 就是对应的epoch数
+
+# 启动visdom
+# python -m visdom.server
